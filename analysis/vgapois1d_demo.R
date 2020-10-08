68d5402a5dad5a4a5dea7e9106fcc18f9803a56d
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-10-08 13:48:00 -0500

    Implemented function compute_logp_pois.

--- analysis/vgapois1d_demo.R
+++ analysis/vgapois1d_demo.R
@@ -1,4 +1,6 @@
 # TO DO: Explain here what this script is for, and how to use it.
+library(pracma)
+source("../code/vgapois.R")
 set.seed(1)
 
 # SIMULATE DATA
@@ -10,15 +12,14 @@ x <- rnorm(n)
 r <- exp(x*b)
 y <- rpois(n,r)
 
-# PLOT LIKELIHOOD SURFACE
-# -----------------------
+# PLOT POSTERIOR SURFACE
+# ----------------------
 # TO DO: Explain here what these lines of code do.
-b <- seq(-2,4,length.out = 1000)
+s0 <- 10
+b <- seq(-1,3,length.out = 1000)
 n <- length(b)
 loglik <- rep(0,n)
-for (i in 1:n) {
-  r <- exp(x*b[i])
-  loglik[i] <- sum(dpois(y,r,log = TRUE))
-}
-plot(b,exp(loglik - max(loglik)),type = "l",lwd = 2,col = "black",
+for (i in 1:n)
+  loglik[i] <- compute_logp_pois(x,y,b[i],s0)
+plot(b,exp(loglik - max(loglik)),type = "l",lwd = 2,col = "dodgerblue",
      xlab = "b",ylab = "relative likelihood")
--- code/vgapois.R
+++ code/vgapois.R
@@ -1,3 +1,18 @@
-vgapois1d <- function (x) {
+# TO DO: Explain here what this function does, and how to use it.
+#
+# This implementation is meant to be "instructive"---that is, I've
+# tried to make the code as simple as possible, with an emphasis on
+# clarity. Very little effort has been devoted to making the
+# implementation efficient, or the code concise.
+vgapois1d <- function (x, y) {
+
+}
+
+# TO DO: Explain here what this function does, and how to use it.
+compute_logp_pois <- function (x, y, b, s0)
+  sum(dpois(y,exp(x*b),log = TRUE)) + dnorm(b,sd = s0^2,log = TRUE)
+
+# TO DO: Explain here what this function does, and how to use it.
+compute_elbo_vgapois1d <- function (x, y) {
 
 }
