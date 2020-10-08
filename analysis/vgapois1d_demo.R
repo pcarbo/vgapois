9a55cf513002c403401cd90249f11095685f9f50
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-10-08 15:03:33 -0500

    Implemented compute_elbo_vgapois1d in vgapois.R.

--- analysis/vgapois1d_demo.R
+++ analysis/vgapois1d_demo.R
@@ -12,14 +12,36 @@ x <- rnorm(n)
 r <- exp(x*b)
 y <- rpois(n,r)
 
-# PLOT POSTERIOR SURFACE
-# ----------------------
+# PLOT ELBO SURFACE
+# -----------------
 # TO DO: Explain here what these lines of code do.
-s0 <- 10
-b <- seq(-1,3,length.out = 1000)
-n <- length(b)
-loglik <- rep(0,n)
+mu   <- seq(-1,2.5,length.out = 50)
+s    <- 10^seq(-3,0,length.out = 50)
+out  <- meshgrid(s,mu)
+S    <- out$X
+MU   <- out$Y
+n    <- length(S)
+elbo <- matrix(0,nrow(S),ncol(S))
 for (i in 1:n)
-  loglik[i] <- compute_logp_pois(x,y,b[i],s0)
-plot(b,exp(loglik - max(loglik)),type = "l",lwd = 2,col = "dodgerblue",
-     xlab = "b",ylab = "relative likelihood")
+  elbo[i] <- compute_elbo_vgapois1d(x,y,s0,MU[i],S[i])
+contour(mu,log10(s),elbo,col = "dodgerblue",nlevels = 24,
+        xlab = "mu",ylab = "s")
+i  <- which.max(elbo)
+mu <- MU[i]
+s  <- S[i]
+points(mu,log10(s),pch = 4,col = "magenta")
+
+# PLOT (EXACT) POSTERIOR SURFACE
+# ------------------------------
+# TO DO: Explain here what these lines of code do.
+s0   <- 10
+b    <- seq(-1,3,length.out = 1000)
+n    <- length(b)
+logp <- rep(0,n)
+qb   <- dnorm(b,MU[i],S[i],log = TRUE)
+for (i in 1:n)
+  logp[i] <- compute_logp_pois(x,y,b[i],s0)
+maxlp <- max(logp)
+plot(b,exp(logp - maxlp),type = "l",lwd = 2,col = "dodgerblue",
+     xlab = "b",ylab = "relative posterior")
+lines(b,qb/max(qb),col = "darkorange")
--- code/vgapois.R
+++ code/vgapois.R
@@ -13,6 +13,6 @@ compute_logp_pois <- function (x, y, b, s0)
   sum(dpois(y,exp(x*b),log = TRUE)) + dnorm(b,sd = s0^2,log = TRUE)
 
 # TO DO: Explain here what this function does, and how to use it.
-compute_elbo_vgapois1d <- function (x, y) {
-
-}
+compute_elbo_vgapois1d <- function (x, y, s0, mu, s)
+  mu*sum(x*y) - sum(exp(x*mu + x^2*s/2)) - sum(lfactorial(y)) +
+    (1 - mu^2/s0 - s/s0 + log(s) - log(s0))/2
